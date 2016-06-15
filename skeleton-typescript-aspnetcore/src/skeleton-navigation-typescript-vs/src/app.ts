0f309e2b49eb39b7341e296a01c32bd49128b197
MartinMason <Martin@softwaremasons.com>
2016-06-14 20:21:13 -0500

    integration of aurelia-authentication
    updated to latest version of aurelia packages; transitioned to new form
    of typings; integrated aurelia-authentication

--- skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/src/app.ts
+++ skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/src/app.ts
@@ -6,9 +6,11 @@ export class App {
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'welcome'], name: 'welcome',        moduleId: 'welcome',        nav: true, title: 'Welcome' },
+      { route: 'users',         name: 'users',          moduleId: 'users',          nav: true, title: 'Github Users' },
+      { route: 'child-router',  name: 'child-router',   moduleId: 'child-router',   nav: true, title: 'Child Router' },
+      { route: 'login',         name: 'login',          moduleId: 'login',          nav: true, title: 'Login' }
+
     ]);
 
     this.router = router;
---
+++ skeleton-typescript-aspnetcore/src/skeleton-navigation-typescript-vs/src/login.ts
@@ -0,0 +1,40 @@
+﻿import {AuthService} from 'aurelia-authentication';
+import {inject} from 'aurelia-framework';
+
+@inject(AuthService)
+export class Login {
+    private auth: AuthService;
+    email: string;
+    password: string;
+    heading: string = 'Login';
+
+    constructor(auth: AuthService) {
+        this.auth = auth;
+    };
+
+    login() {
+        let loginOptions = {
+            username: this.email,
+            password: this.password,
+            grant_type: 'password',
+            scope: 'openid profile email'
+        };
+        return this.auth.login(loginOptions)
+            .then(response => {
+                console.log("success logged " + response);
+            })
+            .catch(err => {
+                err.json().then(function (e) {
+                    console.log("login failure : " + e.message);
+                });
+
+            });
+    };
+
+    authenticate(name) {
+        return this.auth.authenticate(name, false, null)
+            .then((response) => {
+            });
+
+    }
+}
